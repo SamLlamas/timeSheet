98bcbd1c34bff9ea26f8d308e302a02df86aefdb
ektravel <38017980+ektravel@users.noreply.github.com>
2018-05-02 14:26:08 -0500

    added moment.js

--- app.js
+++ app.js
@@ -17,33 +17,27 @@ var database = firebase.database();
 // Initial Values
 var name = "";
 var role = "";
-var startDate = 0;
-var monthlyRate = "";
+var startDate = "01/01/2001";
+var startFormat = "MM/DD/YYYY";
+var convertedDate = moment(startDate, startFormat);
+var monthsWorked = Math.abs(moment(convertedDate).diff(moment(), "months"));
+var monthlyRate = 2000;
+var totalBilled = (monthlyRate * monthsWorked);
+
+console.log(monthsWorked);
+console.log(totalBilled);
+
+
 // Capture Button Click
 $("#add-user").on("click", function (event) {
     event.preventDefault();
 
     console.log("click");
 
-    name = $("#name-input")
-        .val()
-        .trim();
-    role = $("#role-input")
-        .val()
-        .trim();
-    // verify "moment" format is correct
-    startDate = moment(
-        $("#start-input")
-        .val()
-        .trim()
-    ).format("MM/DD/YY");
-    startDate = $("#start-input").val().trim();
-    monthlyRate = $("#rate-input")
-        .val()
-        .trim();
-
-    console.log(startDate);
-
+    name = $("#name-input").val().trim();
+    role = $("#role-input").val().trim();
+    startDate = moment($("#start-input").val().trim()).format("MM/DD/YYYY");
+    monthlyRate = $("#rate-input").val().trim();
 
     database.ref().push({
         name: name,
@@ -53,29 +47,28 @@ $("#add-user").on("click", function (event) {
         dateAdded: firebase.database.ServerValue.TIMESTAMP
     });
 
-    // console.log(name.name);
-    // console.log(role.role);
-    // console.log(start.startDate);
-    // console.log(rate.monthlyRate);
+    console.log(name);
+    console.log(role);
+    console.log(startDate);
+    console.log(monthlyRate);
 });
-// database.ref().on("child_added", function(childSnapshot) {
+database.ref().on("child_added", function (childSnapshot) {
 
-//     console.log(childSnapshot.val());  
+    console.log(childSnapshot.val());
 
-//     //Logging Employee Info
-//     console.log(childSnapshot.val().name);
-//     console.log(childSnapshot.val().role);
-//     console.log(childSnapshot.val().start);
-//     console.log(childSnapshot.val().rate);
+    //Logging Employee Info
+    console.log(childSnapshot.val().name);
+    console.log(childSnapshot.val().role);
+    console.log(childSnapshot.val().start);
+    console.log(childSnapshot.val().rate);
 
-//     //Add Employee Info into the table 
-//     $("#table-headers>tbody").append("<tr><td>" + (childSnapshot.val().name) + "</td><td>" + (childSnapshot.val().role) + "</td><td>" +
-//     (childSnapshot.val().start) + "</td><td>" + monthsWorked + "</td><td>" + (childSnapshot.val().rate) + "</td><td>" + totalBilled + "</td></tr>");
-        
-
-
-    
-// });
+    //Add Employee Info into the table 
+    $("#table-headers>tbody").append("<tr><td>" + (childSnapshot.val().name) + "</td><td>" + (childSnapshot.val().role) + "</td><td>" +
+        (childSnapshot.val().start) + "</td><td>" + monthsWorked + "</td><td>" + (childSnapshot.val().rate) + "</td><td>" + totalBilled + "</td></tr>");
 
 
-    
+
+
+});
+
+
