aaed13f6e95c9458599508d0d4e73836e8f744a5
ColePresnell <cole.presnell@gmail.com>
2018-05-02 10:04:04 -0500

    initial js

---
+++ app.js
@@ -0,0 +1,32 @@
+
+
+
+var database = firebase.database();
+
+    // Initial Values
+    var name = "";
+    var role = "";
+    var startDate = 0;
+    var monthlyRate = "";
+
+    // Capture Button Click
+    $("#add-user").on("click", function(event) {
+      // Don't refresh the page!
+      event.preventDefault();
+
+      // YOUR TASK!!!
+      // Code in the logic for storing and retrieving the most recent user.
+      // Don't forget to provide initial data to your Firebase database.
+      name = $("#name-input").val().trim();
+      email = $("#email-input").val().trim();
+      age = $("#age-input").val().trim();
+      comment = $("#comment-input").val().trim();
+
+      database.ref().push({
+        name: name,
+        email: email,
+        age: age,
+        comment: comment
+      });
+
+    });
